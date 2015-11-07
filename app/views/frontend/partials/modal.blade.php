43edca169beca8378392310f1f8f88e98f84db50
netfox01 <eissa.soubhi@gmail.com>
2015-11-07 22:36:25 +0000

    photo's buttons edit, download, link and  add to favorites done. confirmation modals dor deleting buttons done. comments issues done. photos' url optimazition for SEO done.

---
+++ app/views/frontend/partials/modal.blade.php
@@ -0,0 +1,19 @@
+<div class="modal fade" id="delete-modal" tabindex="-1" role="dialog" aria-labelledby="delete-modalLabel" aria-hidden="true">
+  <div class="modal-dialog modal-sm">
+    <div class="modal-content">
+      <div class="modal-header">
+        <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
+        <h4 class="modal-title" id="delete-modalLabel">Confirmation de suppression !</h4>
+      </div>
+      <div class="modal-body">
+        êtes vous sûre de vouloire supprimer?
+      </div>
+      <div class="modal-footer">
+        <button type="button" class="btn btn-default" data-dismiss="modal"><i class="fa fa-undo"></i> Annuler</button>
+        {{Form::open(array('method' => 'DELETE','class' => 'commit-delete'))}}
+            <button type="submit" class="btn btn-danger delete-modal-submit" ><i class="fa fa-times"></i> Supprimer</button>
+        {{Form::close()}}
+      </div>
+    </div>
+  </div>
+</div>
