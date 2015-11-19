9d010701d6e737394b31075a526db5a536ba637e
netfox01 <eissa.soubhi@gmail.com>
2015-11-19 01:54:13 +0000

    adding custom 401 page + permessions filters

---
+++ app/views/frontend/article/alternative-401-page.blade.php
@@ -0,0 +1,13 @@
+@extends('frontend.layouts.main')
+  @section('content')
+        <div class="content container-fluid">
+            <div class="row fadeIn wow animated">
+              <div class="col-md-12 text-article">
+                <p style="text-align:center"><span style="color:#DD4814"><span style="font-size:200px">401</span></span></p>
+                    <p style="text-align:center"><span style="color:#DD4814"><span style="font-size:200px"><img alt="sad" src="http://cdn.ckeditor.com/4.5.1/full/plugins/smiley/images/angel_smile.png" style="height:23px; width:23px" title="sad" /></span></span></p>
+
+                    <p style="text-align:center"><span style="color:#DD4814"><span style="font-size:20px">Désolé..page non autorisée !!</span></span></p>
+              </div>
+            </div>
+        </div>
+  @stop
